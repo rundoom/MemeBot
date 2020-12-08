3e493a8d5ab7157a10c990755fe47d75e6c262ac
borisov <rundoom1337@gmail.com>
2020-12-08 17:23:37 +0400

    Empty channelsFrom fills with config.personalChatId

--- src/main/kotlin/org/rndd/Main.kt
+++ src/main/kotlin/org/rndd/Main.kt
@@ -1,6 +1,7 @@
 package org.rndd
 
 import kotlinx.dnq.XdModel
+import kotlinx.dnq.query.asIterable
 import kotlinx.dnq.util.initMetaData
 import org.rndd.tgbot.initTelegramBot
 import org.rndd.tgcore.initTgCore
@@ -10,6 +11,12 @@ import kotlin.concurrent.thread
 fun main() {
     XdModel.registerNodes(XdMinithumbnail, XdChat, XdChatState, XdSticker)
     initMetaData(XdModel.hierarchy, xodusStore)
+
+    xodusStore.transactional {
+        XdMinithumbnail.all().asIterable()
+            .filter { it.channelsFrom.isEmpty() }
+            .forEach { it.channelsFrom.add(config.personalChatId) }
+    }
 
     thread { initTelegramBot() }
     initTgCore()
